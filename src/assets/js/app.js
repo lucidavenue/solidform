3b7d5244e97c42f9db7597d1d5905c1a3d7e3356
Vedran Jaic <lucidavenue@gmail.com>
2022-07-07 01:59:42 +0200

    scroll-to, nav, clients, digital services

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -46,6 +46,39 @@ $(".navbar-toggler").click(function() {
 	$(".navbar-fixed-top").toggleClass("navbar-open");
 });
 
+
+
+
+
+
+
+// --- [ SCROLL TO TOP ]
+// init controller
+var controller = new ScrollMagic.Controller();
+// build tween
+var tween = TweenMax.from("#animate", 0.5, {autoAlpha: 0, scale: 0.7});
+// build scene
+var scene = new ScrollMagic.Scene({
+	triggerElement: "a#top",
+	duration: 200,
+	triggerHook: "onLeave"
+})
+.setTween(tween)
+// .addIndicators() // add indicators (requires plugin)
+.addTo(controller);
+
+controller.scrollTo(function(target) {
+
+	TweenMax.to(window, 0.5, {
+	  scrollTo : {
+		y : target, // scroll position of the target along y axis
+		autoKill : true // allows user to kill scroll action smoothly
+	  },
+	  ease : Cubic.easeInOut
+	});
+  
+  });
+
 // bind scroll to anchor links
 $(document).on("click", ".scroll-to[href^='#']", function (e) {
 	var id = $(this).attr("href");
